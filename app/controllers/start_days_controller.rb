00b6cd555d7a8ce8ec9abf14c54a5f86fc1fd466
suzy1031 <kohei.sugawara.1031@gmail.com>
2020-01-04 16:40:47 +0900

    add new function in controller.rb

--- app/controllers/start_days_controller.rb
+++ app/controllers/start_days_controller.rb
@@ -3,7 +3,28 @@ class StartDaysController < ApplicationController
     if user_signed_in?
       @start_day = StartDay.where(user_id: current_user.id)
       @count_time = CountTime.where(user_id: current_user.id).order('updated_at DESC').limit(5)
-      @count_hour = @count_time.all.sum(:count_hour)
+      @count_hour = @count_time.sum(:count_hour)
+
+      continued = CountTime.where(user_id:current_user.id)
+      @Durationdays = 0
+
+      if continued.select("id").where(created_at: 0.day.ago.all_day).blank?
+          i = 1
+      else
+          i = 0
+      end
+
+      while continued.length do
+        unless continued.select("id").where(created_at: i.day.ago.all_day).blank?
+          @Durationdays += 1
+          i += 1
+        else
+          countinuedExit = false
+        end
+        if countinuedExit == false
+          break
+        end
+      end
     end
   end
 
